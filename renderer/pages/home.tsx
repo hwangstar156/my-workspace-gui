6d5e8c47ada6e265abe197d3e54a6dca2d9e85d5
hwangstar156 <hwangstar156@gmail.com>
2025-01-13 01:59:58 +0900

    chore: eslint및 prettier 적용

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,32 +1,36 @@
 import { useEffect, useState } from 'react'
 
 export default function HomePage() {
-  const [globalNpmrc, setGlobalNpmrc] = useState<string[]>([]);
-  const [projectNpmrc, setProjectNpmrc] = useState<string[]>([]);
+  const [globalNpmrc, setGlobalNpmrc] = useState<string[]>([])
+  const [projectNpmrc, setProjectNpmrc] = useState<string[]>([])
 
   useEffect(() => {
     const getNpmrc = async () => {
-      const homeDir = await window.api.getHomeDir();
+      const homeDir = await window.api.getHomeDir()
 
-      const globalResult = await window.npmrcAPI.readNpmrc(`${homeDir}/.npmrc`);
-      const projectResult = await window.npmrcAPI.readNpmrc(`./.npmrc`);
+      const globalResult = await window.npmrcAPI.readNpmrc(`${homeDir}/.npmrc`)
+      const projectResult = await window.npmrcAPI.readNpmrc(`./.npmrc`)
 
-      const globalContent = globalResult.content.split('\n');
-      const projectContent = projectResult.content.split('\n');
+      const globalContent = globalResult.content.split('\n')
+      const projectContent = projectResult.content.split('\n')
 
-      setGlobalNpmrc(globalContent);
-      setProjectNpmrc(projectContent);
+      setGlobalNpmrc(globalContent)
+      setProjectNpmrc(projectContent)
     }
 
-    getNpmrc();
+    getNpmrc()
   }, [])
 
   return (
     <>
       <div>global npmrc</div>
-      {globalNpmrc.map((npmrc) => <div>{npmrc}</div>)}
+      {globalNpmrc.map((npmrc) => (
+        <div>{npmrc}</div>
+      ))}
       <div>local npmrc</div>
-      {projectNpmrc.map((npmrc) => <div>{npmrc}</div>)}
+      {projectNpmrc.map((npmrc) => (
+        <div>{npmrc}</div>
+      ))}
     </>
   )
 }
