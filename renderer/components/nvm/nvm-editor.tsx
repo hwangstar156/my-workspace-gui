52ffa031bdd31b325cfa7a7fa65696d8b283c85d
hwangstar156 <hwangstar156@gmail.com>
2025-01-16 01:37:16 +0900

    feat: 현재 node version에 대한 상태 추가

--- renderer/components/nvm/nvm-editor.tsx
+++ renderer/components/nvm/nvm-editor.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { SwitchCase } from '../shared/switch-case'
 
 export function NvmEditor() {
   const [versions, setVersions] = useState([])
@@ -6,6 +7,8 @@ export function NvmEditor() {
   useEffect(() => {
     async function fetchFilteredVersions() {
       try {
+        const currentVersion = (await window.nvmAPI.getCurrentVersion()) as string
+        console.log(currentVersion)
         const remoteVersions = (
           (await window.nvmAPI.command('nvm ls-remote --no-colors')) as string
         )
@@ -24,16 +27,25 @@ export function NvmEditor() {
 
         const versions = remoteVersions
           .map((version) => {
-            const status = installedVersions.includes(version) ? 'installed' : 'not-installed'
+            const status = (function () {
+              if (version.includes(currentVersion)) {
+                return 'current'
+              }
+
+              if (installedVersions.includes(version)) {
+                return 'installed'
+              }
+
+              return 'not-installed'
+            })()
+
             return { version, status }
           })
           .sort((a, b) => {
-            if (a.status === 'installed' && b.status !== 'installed') {
-              return -1
-            }
+            const priority = { current: 0, installed: 1, 'not-installed': 2 }
 
-            if (a.status !== 'installed' && b.status === 'installed') {
-              return 1
+            if (priority[a.status] !== priority[b.status]) {
+              return priority[a.status] - priority[b.status]
             }
 
             const versionToNumber = (version: string) =>
@@ -41,7 +53,6 @@ export function NvmEditor() {
             return versionToNumber(b.version) - versionToNumber(a.version)
           })
 
-        console.log(versions)
         setVersions(versions)
       } catch (error) {
         console.error('버전 정보를 가져오지 못했습니다.', error)
@@ -72,7 +83,16 @@ export function NvmEditor() {
       <ul>
         {versions.map(({ version, status }) => (
           <li key={version} onClick={() => handleVersionSelect(version, status)}>
-            {version} ({status === 'installed' ? '사용하기' : '설치 후 사용하기'})
+            {version}
+            <SwitchCase
+              value={status}
+              caseBy={{
+                installed: <span style={{ color: 'green' }}> (installed)</span>,
+                current: <span style={{ color: 'blue' }}> (current)</span>,
+                'not-installed': <span style={{ color: 'red' }}> (not installed)</span>,
+              }}
+              defaultComponent={null}
+            />
           </li>
         ))}
       </ul>
