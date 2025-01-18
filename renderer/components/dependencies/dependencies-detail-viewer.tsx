ee8c0527e45e13e20e3b79f62340bb926ba57e44
hwangstar156 <hwangstar156@gmail.com>
2025-01-19 03:47:02 +0900

    feat: npm list, npm view를 통해서 dependency들을 분석할 수 있도록 처리

--- renderer/components/dependencies/dependencies-detail-viewer.tsx
+++ renderer/components/dependencies/dependencies-detail-viewer.tsx
@@ -1,7 +1,40 @@
 import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 
 export function DependenciesDetailViewer() {
+  const [dependencies, setDependencies] = useState<{ name: string; version: string }[]>([])
   const router = useRouter()
+  const id = router.query.id
 
-  return <div>{router.query.id}</div>
+  useEffect(() => {
+    const fetchAndSetDependencies = async () => {
+      const projectPaths: {
+        id: string
+        path: string
+      }[] = await window.storeAPI.get('projectPath')
+
+      const currentProjectPath = projectPaths.find((projectPath) => projectPath.id === id)
+
+      if (!currentProjectPath) {
+        return
+      }
+
+      const dependencies = await window.projectAPI.getDependencies(currentProjectPath.path)
+
+      setDependencies(dependencies)
+    }
+
+    fetchAndSetDependencies()
+  }, [])
+
+  return (
+    <div>
+      {dependencies.map((dependency) => (
+        <div key={dependency.name}>
+          <span>{dependency.name}</span>
+          <span>{dependency.version}</span>
+        </div>
+      ))}
+    </div>
+  )
 }
